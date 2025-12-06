4dcbe3d186d8181b89dcc6c67c324a95d307ca98
Wilton Batiquin <cbasher24@gmail.com>
2025-12-07 00:51:06 +0800

    initial commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+
+  console.log('Testing');
+
+  return (
+    <main>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome to this NextJS Course!</h1>
+      <p>🔥 Let&apos;s get started in the hause! 🔥</p>
+    </main>
+  );
+}
