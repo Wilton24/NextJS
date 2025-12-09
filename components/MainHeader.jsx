e2ad86d5b475afe9023e477277dc70cf042f8568
Wilton Batiquin <cbasher24@gmail.com>
2025-12-09 13:59:46 +0800

    Refactor homepage layout and styling; add MainHeader component and associated styles

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import styles from "./main-header.module.css";
+import Image from "next/image";
 
 
 export default function MainHeader() {
     return (
-        <header className={styles.logo}>
-            <Link href="/">
-                <img src={logoImg.src} alt="NextLevel Food Logo" /></Link>
-            Next Level Food
-            <nav>
+        <header className={styles.header}>
+            <Link className={styles.logo} href="/">
+                <Image src={logoImg} alt="NextLevel Food Logo" priority /></Link>
+            <h2>NextLevel Food</h2>
+            <nav className={styles.nav}>
                 <ul>
                     <li>
                         <Link href="/meals">Browse Meals</Link>
