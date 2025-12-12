9e259d1da11ebb6dd597bbd5130dc422d6f4aa3d
Wilton Batiquin <cbasher24@gmail.com>
2025-12-12 13:53:20 +0800

    Added the "Not found" page

---
+++ not-found.js
@@ -0,0 +1,8 @@
+
+
+export default function NotFoundPage() {
+    return <main className="not-found">
+        <h1>Not Found</h1>
+        <p>Unfortunately, the requested meal could not be found.</p>
+    </main>
+}
